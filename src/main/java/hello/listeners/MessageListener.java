275764374aae811dd16c754ec2872933646cfd71
Jorge García Marín <Jorge Garca Marn@Swifty>
2018-03-10 19:42:04 +0100

    added sender package

--- src/main/java/hello/listeners/MessageListener.java
+++ src/main/java/hello/listeners/MessageListener.java
@@ -5,6 +5,8 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 
+import java.util.concurrent.CountDownLatch;
+
 import javax.annotation.ManagedBean;
 
 /**
@@ -14,10 +16,17 @@ import javax.annotation.ManagedBean;
 public class MessageListener {
 
     private static final Logger logger = Logger.getLogger(MessageListener.class);
+    
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public CountDownLatch getLatch() {
+      return latch;
+    }
 
     @KafkaListener(topics = "exampleTopic")
     public void listen(String data) {
         logger.info("New message received: \"" + data + "\"");
+        latch.countDown();
     }
 
 
