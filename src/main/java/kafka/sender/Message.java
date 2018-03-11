b120c6ea2f1fe6a70e2cba998beb04c7bf2065bf
sergiosantano <sergiosantano96@gmail.com>
2018-03-11 20:54:08 +0100

    Apache kafka structure done, but not working

---
+++ src/main/java/kafka/sender/Message.java
@@ -0,0 +1,21 @@
+package kafka.sender;
+
+public class Message {
+
+	String message;
+	
+	public Message() {
+		
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
