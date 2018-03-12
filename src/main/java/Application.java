2dd89b3d534dc71c22d04e22fa6464b1513e6403
JorgeGarciaMarin <0xjorgegm@gmail.com>
2018-03-12 08:41:52 +0100

    Added mock sender and receiver

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,25 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import kafka.sender.Sender;
+
 @SpringBootApplication
-@ComponentScan({"controller"})
-public class Application {
+@ComponentScan({"controller", "kafka"})
+public class Application implements CommandLineRunner {
 
     public static void main(String[] args) {
     	SpringApplication.run(Application.class, args);
+    }
+    @Autowired
+    private Sender sender;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        sender.send("Spring Kafka Producer and Consumer Example");
+        sender.send("Example 2");
+
     }
 }
